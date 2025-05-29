752b8900bdee4f6f1b1964378f83513b98c59136
dwing <dwing4g@gmail.com>
2025-05-29 19:15:31 +0800

    translation: re-translate MAP04~06

---
+++ translation/trans_me.bat
@@ -0,0 +1,30 @@
+@echo off
+setlocal
+pushd %~dp0
+
+luajit openai.lua openai.trans.lua MAP01.me    MAP01.mc
+luajit openai.lua openai.trans.lua MAP02.me    MAP02.mc
+luajit openai.lua openai.trans.lua MAP03.me    MAP03.mc
+luajit openai.lua openai.trans.lua MAP04.me    MAP04.mc
+luajit openai.lua openai.trans.lua MAP05.me    MAP05.mc
+luajit openai.lua openai.trans.lua MAP06.me    MAP06.mc
+luajit openai.lua openai.trans.lua MAP07.me    MAP07.mc
+luajit openai.lua openai.trans.lua MAP08.me    MAP08.mc
+luajit openai.lua openai.trans.lua MAP09.me    MAP09.mc
+luajit openai.lua openai.trans.lua MAP10.me    MAP10.mc
+luajit openai.lua openai.trans.lua MAP11.me    MAP11.mc
+luajit openai.lua openai.trans.lua MAP12.me    MAP12.mc
+luajit openai.lua openai.trans.lua MAP13.me    MAP13.mc
+luajit openai.lua openai.trans.lua MAP14.me    MAP14.mc
+luajit openai.lua openai.trans.lua MAP15.me    MAP15.mc
+luajit openai.lua openai.trans.lua MAP16.me    MAP16.mc
+luajit openai.lua openai.trans.lua MAP17.me    MAP17.mc
+luajit openai.lua openai.trans.lua MAP18.me    MAP18.mc
+luajit openai.lua openai.trans.lua MAP19.me    MAP19.mc
+luajit openai.lua openai.trans.lua MAP20.me    MAP20.mc
+luajit openai.lua openai.trans.lua MAP21.me    MAP21.mc
+luajit openai.lua openai.trans.lua MAP22.me    MAP22.mc
+luajit openai.lua openai.trans.lua MAP31.me    MAP31.mc
+luajit openai.lua openai.trans.lua TITLEMAP.me TITLEMAP.mc
+
+pause
